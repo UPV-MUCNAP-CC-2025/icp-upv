f0825c022dbc9049174fd9138e4a470230e96652
hector-medina <hector.medina.cabanelas@gmail.com>
2026-02-09 18:35:32 +0100

    refactor

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
-  // carga .env, .env.local, etc.
   const env = loadEnv(mode, process.cwd(), '')
 
   const target = env.VITE_API_BASE_URL
