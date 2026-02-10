ed3c20858f31b726cc07f591e34df6b35cd9d328
hector-medina <hector.medina.cabanelas@gmail.com>
2026-02-10 03:09:08 +0100

    add version for lambda

--- infrastructure/backend/20-lamba-deployment.sh
+++ infrastructure/backend/20-lamba-deployment.sh
@@ -7,6 +7,12 @@ deploy_lambda() {
   echo "🚀 Deploying stack: $CFN_LAMBDA_STACK"
   echo "=================================================="
 
+  LAMBDA_S3_VERSION="$(aws s3api head-object \
+    --bucket "$CFN_BUCKET_STACK" \
+    --key "$LAMBDA_S3_KEY" \
+    --query VersionId \
+    --output text)"
+
   aws cloudformation deploy \
     --template-file $CFN_LAMBDA_TEMPLATE \
     --stack-name $CFN_LAMBDA_STACK \
@@ -14,6 +20,7 @@ deploy_lambda() {
     --parameter-overrides \
       ToDoTableName="$DYNAMODB_TABLE_NAME" \
       LambdaCodeBucket="$CFN_BUCKET_STACK" \
-      LambdaCodeKey="$LAMBDA_S3_KEY"
+      LambdaCodeKey="$LAMBDA_S3_KEY" \
+      LambdaCodeVersion="$LAMBDA_S3_VERSION"
 
 }
