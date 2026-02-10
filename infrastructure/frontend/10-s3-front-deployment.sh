a22a6b7a2c5ec758327c647679ae276f405737e7
hector-medina <hector.medina.cabanelas@gmail.com>
2026-02-10 04:03:22 +0100

    update front deployment

--- infrastructure/frontend/10-s3-front-deployment.sh
+++ infrastructure/frontend/10-s3-front-deployment.sh
@@ -11,13 +11,14 @@ create_frontend() {
     --template-file "$CFN_FRONT_TEMPLATE" \
     --stack-name "$CFN_FRONT_STACK"
 
-  API_URL="$(aws cloudformation describe-stacks \
+  export VITE_API_BASE_URL="$(aws cloudformation describe-stacks \
     --stack-name "$CFN_APIGW_STACK" \
     --query "Stacks[0].Outputs[?OutputKey=='TodosBaseUrl'].OutputValue | [0]" \
     --output text)"
 
+  echo "VITE_API_BASE_URL=$VITE_API_BASE_URL"
+
   (cd "$ROOT_DIR/app/frontend" \
-    && export VITE_API_BASE_URL="$API_URL/prod" \
     && npm ci \
     && npm run build)
     
