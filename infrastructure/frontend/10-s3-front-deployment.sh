52215de6f2645cbcce37d3ae0463f73fdc67a852
hector-medina <hector.medina.cabanelas@gmail.com>
2026-02-10 04:17:08 +0100

    update uri

--- infrastructure/frontend/10-s3-front-deployment.sh
+++ infrastructure/frontend/10-s3-front-deployment.sh
@@ -11,16 +11,14 @@ create_frontend() {
     --template-file "$CFN_FRONT_TEMPLATE" \
     --stack-name "$CFN_FRONT_STACK"
 
-  export VITE_API_BASE_URL="$(aws cloudformation describe-stacks \
+  API_URL="$(aws cloudformation describe-stacks \
     --stack-name "$CFN_APIGW_STACK" \
     --query "Stacks[0].Outputs[?OutputKey=='TodosBaseUrl'].OutputValue | [0]" \
     --output text)"
 
-  echo "VITE_API_BASE_URL=$VITE_API_BASE_URL"
-
   (cd "$ROOT_DIR/app/frontend" \
     && npm ci \
-    && npm run build)
+    && VITE_API_BASE_URL="$API_URL" npm run build)
     
   aws s3 sync $ROOT_DIR/app/frontend/dist "s3://$CFN_FRONT_STACK" --delete
 
