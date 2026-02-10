1d6e15e257e9058634fadfec96405414778b008f
hector-medina <hector.medina.cabanelas@gmail.com>
2026-02-10 03:40:36 +0100

    update front stack to get apigw endpoint

--- infrastructure/frontend/10-s3-front-deployment.sh
+++ infrastructure/frontend/10-s3-front-deployment.sh
@@ -11,8 +11,16 @@ create_frontend() {
     --template-file "$CFN_FRONT_TEMPLATE" \
     --stack-name "$CFN_FRONT_STACK"
 
-  (cd "$ROOT_DIR/app/frontend" && npm ci && npm run build)
+  API_URL="$(aws cloudformation describe-stacks \
+    --stack-name "$CFN_APIGW_STACK" \
+    --query "Stacks[0].Outputs[?OutputKey=='TodosBaseUrl'].OutputValue | [0]" \
+    --output text)"
 
-  aws s3 sync app/frontend/dist "s3://$CFN_FRONT_STACK" --delete
+  (cd "$ROOT_DIR/app/frontend" \
+    && export VITE_API_BASE_URL="$API_URL" \
+    && npm ci \
+    && npm run build)
+    
+  aws s3 sync $ROOT_DIR/app/frontend/dist "s3://$CFN_FRONT_STACK" --delete
 
 }
