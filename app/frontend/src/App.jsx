ce10f1076d4bd0a2dc60ae8b89dae764f0d210ee
hector-medina <hector.medina.cabanelas@gmail.com>
2026-02-10 04:27:58 +0100

    update path

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const API_URL = import.meta.env.VITE_API_BASE_URL;
+const API_URL = import.meta.env.VITE_API_BASE_URL+'/todos';
 
 function App() {
   const [todos, setTodos] = useState([]);
